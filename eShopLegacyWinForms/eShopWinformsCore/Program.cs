185e26e64e7c2c1eabdde69a460877aa0a81752c
ridomin <ridomin@live.com>
2018-09-13 21:03:08 -0700

    add nc3 project

---
+++ eShopLegacyWinForms/eShopWinformsCore/Program.cs
@@ -0,0 +1,34 @@
+﻿using eShopWinForms.Controllers;
+using eShopWinForms.eShopServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eShopWinForms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            CatalogView catalogView = new CatalogView();
+            ICatalogService service = new eShopServiceReference.CatalogServiceClient(
+                new System.ServiceModel.BasicHttpBinding(), 
+                new System.ServiceModel.EndpointAddress(
+                    System.Configuration.ConfigurationManager.AppSettings["ServiceURL"]));
+            CatalogController catalogController = new CatalogController(service, catalogView);
+
+            catalogController.LoadView();
+            catalogView.ShowDialog();
+        }
+
+    }
+}
